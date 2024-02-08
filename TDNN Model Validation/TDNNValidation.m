c531144385c12d50655b86ec5ded08a17084de63
Stoniestbike82 <118928076+Stoniestbike82@users.noreply.github.com>
2024-02-08 13:52:41 +0000

    It still dosen't work buttttttt

--- TDNN Model Validation/TDNNValidation.m
+++ TDNN Model Validation/TDNNValidation.m
@@ -1,7 +1,31 @@
-% wholeData = readtable("EMG-data.csv");
+%% Get Data
+%Import Excel File
+filename='test1.csv';
+data=readmatrix(filename,'NumHeaderLines',1);
 
-rf = rowfilter(wholeData);
+%Assign to variable
+%inputMatrix=array2timetable(data(:,2:9),"RowTimes",seconds(data(:,1)/1000))
+x=[data(:,1:9)]';
+X=con2seq(x);
+%needs to be fixed so it dosen't hurt peoples brains
+%X=combine(data(:,1),data(:,2),data(:,3),data(:,4),data(:,5),data(:,6),data(:,7),data(:,8),data(:,9));
+T=[data(:,1) data(:,10)]';
+T=con2seq(T);
 
-for i=1:36
-    writetable(wholeData(rf.label == i,:),strcat('test',int2str(i),'.csv'));
-end 
+
+%% Create TDNN 
+delay = 1:2;
+neurons =10;
+
+net=timedelaynet(delay,neurons);
+net=configure(net,X,T);
+net.numinputs = 8;
+net.trainParam.epochs=30;
+net=train(net,X,T);
+
+%% Predictions
+outputs=net(X); 
+
+%% Evaluation
+
+performance=perform(net,T,outputs); %needs to be altered to test it on untrained data
