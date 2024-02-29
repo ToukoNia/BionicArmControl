01d1f6de4478df32c236343401dcec020351db1e
ToukoNia <88745518+ToukoNia@users.noreply.github.com>
2024-02-29 18:39:44 +0000

    Fixed a typo

--- TDNN Model Validation/TensorFlow/Loadfile.py
+++ TDNN Model Validation/TensorFlow/Loadfile.py
@@ -1,8 +1,8 @@
-import os
 import tensorflow as tf
 from tensorflow import keras
 from emgTDNNNetwork import model
 
-model=tf.keras.models.load_model('emgTDNNNetwork/model.py')
-probability_model=tf.keras.Sequential([model,tf.keras.layers.Softmax()])
-predictions = probability_model.predict(testData)
+model=tf.keras.models.load_model('emgTDNNNetwork/model.py')                         #gets the model from the associated folder
+testData=0                                                                          #loads test data
+probability_model=tf.keras.Sequential([model,tf.keras.layers.Softmax()])            #creates the model
+predictions = probability_model.predict(testData)                                   #uses the model to predict
