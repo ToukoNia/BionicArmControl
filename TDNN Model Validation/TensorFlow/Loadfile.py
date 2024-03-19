d563f51454173dc44bee85c6ddaddcfe2309f46a
ToukoNia <88745518+ToukoNia@users.noreply.github.com>
2024-03-19 10:41:43 +0000

    Update Loadfile.py

--- TDNN Model Validation/TensorFlow/Loadfile.py
+++ TDNN Model Validation/TensorFlow/Loadfile.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
+#import csv
 
 def PredictCurrentSignal(threshold):
+    stream = "placeholder"
     model=tf.keras.models.load_model('emgTDNNNetwork/model.py')                         #gets the model from the associated folder
-    testData=0                                                                          #loads test data
+    testData=takeSample(stream)                                                                         #loads test data
     probability_model=tf.keras.Sequential([model,tf.keras.layers.Softmax()])            #creates the model
     predictions = probability_model.predict(testData)                                   #uses the model to predict 
     bestPrediction = max(predictions)                                    #finds the index of the value with the highest probability
@@ -14,5 +17,20 @@ def PredictCurrentSignal(threshold):
         return 999
 
 def takeSample(inputDataStream):
-    pass
+    batchSize=256                                                       #sets the batch size for the ANN
+    a = np.genfromtxt(inputDataStream, delimiter=',')
+    return a[-batchSize:]                                              #takes in the last 200 data parts. Need to alter this into a way that does not take the data into memory
+    """
+
+    lines=list()                                                        #has to be a better way
+    with open('csv01.csv') as f:    
+        for line in csv.reader(f):
+            lines.append(line)
+            if len(lines) > batchsize:
+                lines.pop(0)
+        """
+
+
+
+
 
