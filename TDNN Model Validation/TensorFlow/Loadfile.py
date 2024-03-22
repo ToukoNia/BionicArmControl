ddc9c0500171446c044924daf4b776b2f14086db
ToukoNia <88745518+ToukoNia@users.noreply.github.com>
2024-03-22 15:30:35 +0000

    Editing python

--- TDNN Model Validation/TensorFlow/Loadfile.py
+++ TDNN Model Validation/TensorFlow/Loadfile.py
@@ -1,20 +1,20 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-#import csv
+import csv
 
 def PredictCurrentSignal(threshold):
-    stream = "placeholder"
-    model=tf.keras.models.load_model('emgTDNNNetwork/model.py')                         #gets the model from the associated folder
-    testData=takeSample(stream)                                                                         #loads test data
+    stream = "test36.csv"
+    model=tf.keras.models.load_model(r'C:\Users\Rayne\Downloads\myModel\myModel\model.py')                         #gets the model from the associated folder
+    testData=readCSV(stream)                                                            #loads test data
     probability_model=tf.keras.Sequential([model,tf.keras.layers.Softmax()])            #creates the model
     predictions = probability_model.predict(testData)                                   #uses the model to predict 
-    bestPrediction = max(predictions)                                    #finds the index of the value with the highest probability
+    bestPrediction = max(predictions)                                                   #finds the index of the value with the highest probability
     if bestPrediction>=threshold:
-        prediction=predictions.index(bestPrediction)                     #finds the index of the prediction, and returns it
+        prediction=predictions.index(bestPrediction)                                    #finds the index of the prediction, and returns it
         return prediction
-    else:                                                                #returns a value meaning no emg signal
-        return 999
+    else:                                                                               #returns a value meaning no emg signal
+        return 999                                                                      #error code
 
 def takeSample(inputDataStream):
     batchSize=256                                                       #sets the batch size for the ANN
@@ -29,8 +29,20 @@ def takeSample(inputDataStream):
             if len(lines) > batchsize:
                 lines.pop(0)
         """
-
-
+def readCSV(stream):
+    with open(stream) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        lines=list()
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                lines.append(row)
+        return lines
+    
+print(PredictCurrentSignal(0.7))
 
 
 
