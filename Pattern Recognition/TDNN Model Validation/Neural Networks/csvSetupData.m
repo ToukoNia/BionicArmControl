8362384cb74e8a2aa03c2dbf4b197b8e943c2c76
elizabethrscott <erscott2@sheffield.ac.uk>
2025-10-31 18:25:23 +0000

    Update csvSetupData.m

--- Pattern Recognition/TDNN Model Validation/Neural Networks/csvSetupData.m
+++ Pattern Recognition/TDNN Model Validation/Neural Networks/csvSetupData.m
@@ -5,7 +5,7 @@ data = readmatrix(csvfile);
 timesample = data(:, 1);  % Time in microseconds
 sensors = data(:, 2:end-1);  % Sensor data (middle columns)
 gesture = data(:, end);  % Gesture labels (end)
-numSensors=size(sensors,2)
+numSensors = size(sensors,2)
 for i=1:min(numSensors)
     ax(i)=subplot(numSensors,1,i);
     plot(sensors(:,i));
@@ -16,9 +16,9 @@ linkaxes(ax,'y')
 idx_start=find(diff([0;gesture]~=0));
 idx_end = [idx_start(2:end) - 1; length(gesture)];
 
-val = categorical(gestures(idx_start),[1 2 3 4 5 6 7], ["Elbow Flex", "Supinate","Rest","Open Hand", "Pronate", "Close Hand", "Elbow Extension"]) %need to map to ours
+val = categorical(gestures(idx_start),[1 2 3 4 5 6 7], ["Elbow Flex", "Supinate", "Rest", "Open Hand", "Pronate", "Close Hand", "Elbow Extension"]) %need to map to ours
 ROI = [idx_start,idx_end]
-lbltable = table(ROI,val);
+lbltable = table(ROI,val)
 
 sig = sensors;
 roiTable = lbltable;
